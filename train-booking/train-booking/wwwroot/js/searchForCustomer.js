ce661f8d2e7e94c4404dfa2563afc1ae0b54b851
Illya <illya-komar@ukr.net>
2020-11-18 21:28:03 +0200

    Update datatable

---
+++ train-booking/train-booking/wwwroot/js/searchForCustomer.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('.datatable-table').DataTable({
+        dom: 'Bfrtip',
+        responsive: false,
+        searching: true,
+        scrollY: false,
+        info: false,
+        paging: false,
+        ordering: true,
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Ukrainian.json"
+        }
+    });
+});
